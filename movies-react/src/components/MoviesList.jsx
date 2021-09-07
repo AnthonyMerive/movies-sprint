b0f5573abe5900019eeb25a316a24ee0327c4a10
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-09-06 23:13:48 -0500

    tratando slider

--- movies-react/src/components/MoviesList.jsx
+++ movies-react/src/components/MoviesList.jsx
@@ -1,34 +1,36 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import MovieCard from './MoviesCards'
+
 import styles from '../styles/MovieList.module.css'
 
 
 export default function MoviesList() {
-    
+
     const [peliculas, setPeliculas] = useState([]);
-    
+
     useEffect(() => {
         fetch("https://api.themoviedb.org/3/discover/movie", {
 
             headers: {
                 Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzVjODMyMTQ5OTcyMzA3YmMzY2I3MjNjNWQ2NWJmNyIsInN1YiI6IjYxMzUwN2VmMGI1ZmQ2MDA4ODc1NmIyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3MHlpc-jN7rwbAduamFy8U76V9e1bfCvUcZut5Clkso",
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzVjODMyMTQ5OTcyMzA3YmMzY2I3MjNjNWQ2NWJmNyIsInN1YiI6IjYxMzUwN2VmMGI1ZmQ2MDA4ODc1NmIyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3MHlpc-jN7rwbAduamFy8U76V9e1bfCvUcZut5Clkso",
                 "Content-Type": "application/json;charset=utf-8",
             },
         })
-        .then(result => result.json())
-        .then(data => setPeliculas(data.results))
-    },[]); 
-  
-    return (
-        <div>
-            <h2 className={styles.estrenos}>🎬 Latest Releases 🎥</h2>
-            <ul className={styles.movieGrid}>  
+            .then(result => result.json())
+            .then(data => setPeliculas(data.results))
+    }, []);
+
+    peliculas.map(movie =>
+    console.log(`https://image.tmdb.org/t/p/w300${movie.poster_path}`))
+    
+    return (<div>
+            <h2 className={styles.estrenos}>Latest Releases 🎬</h2>
+            <ul className={styles.movieGrid}>
                 {peliculas.map(movie =>
-                    <MovieCard key={movie.id}  movie={movie}/>
-                    )}
+                    <MovieCard key={movie.id} movie={movie} />
+                )}
             </ul>
-        </div>
-    )
+        </div>)
 }
